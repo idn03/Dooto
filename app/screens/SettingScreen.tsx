d92914eff14cf749beb811972b601c59cd77a4c2
idn03 <duyy05568@gmail.com>
2024-12-19 20:16:33 +0700

    complete setting screen

--- app/screens/SettingScreen.tsx
+++ app/screens/SettingScreen.tsx
@@ -1,19 +1,72 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Image, Pressable } from 'react-native';
+import {
+    NavigationProp,
+    useNavigation,
+} from '@react-navigation/native';
+import { RootStackParamList } from '../Router';
+import { CustomText, Icon } from '../components';
+import DootoImgs from '../../assets/assets';
 
 const SettingScreen = () => {
+    const navigation: NavigationProp<RootStackParamList> = useNavigation();
+
     return (
-        <View style={styles.background}>
-            <Text>Hello, this is Setting Screen</Text>
+        <View style={ styles.background }>
+            <Pressable style={ styles.backHomeBtn } onPress={() => navigation.navigate('Home')}>
+                < Icon source={DootoImgs.icons.home} />
+            </Pressable>
+            <View style={ styles.setCenter }>
+                < Image style={ styles.settingIcon } source={DootoImgs.icons.setting} />
+                <CustomText style={ styles.title }>SETTING</CustomText>
+            </View>
+            <View style={{marginHorizontal: 60}}>
+                <CustomText style={{marginTop: 40}}>This is Setting Screen!</CustomText>
+                <CustomText style={{marginTop: 10}}>
+                    Dooto's Settings screen gives you control over how you interact with the platform. 
+                    You can customize your privacy, connect to other accounts, and customize the app's 
+                    interface to your personal preferences.
+                </CustomText>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     background: {
-        flex: 1,
         backgroundColor: '#B4B4B4',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
+        height: '100%',
+        paddingTop: 150
     },
+    setCenter: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    backHomeBtn: {
+        width: 60,
+        backgroundColor: '#333',
+        position: 'relative',
+        left: 0,
+        top: -80,
+        padding: 15,
+        borderTopRightRadius: 30,
+        borderBottomRightRadius: 30,
+        boxShadow: '0 1 2 2 rgba(0, 0, 0, 0.25)',
+    },
+    settingIcon: {
+        height: 80,
+        width: 80,
+    },
+    title: {
+        color: '#F2EFE5',
+        fontSize: 40,
+        fontWeight: 700,
+        textShadowColor: 'rgba(0, 0, 0, 0.25)',
+        textShadowRadius: 3,
+        textShadowOffset: {width: 3, height: 3},
+    }
 });
 
 export default SettingScreen;
--- assets/assets.ts
+++ assets/assets.ts
@@ -1,6 +1,7 @@
 const DootoImgs = {
     logo: require('./logo.png'),
     icons: {
+        home: require('./icons/home.png'),
         notify: require('./icons/notify.png'),
         setting: require('./icons/setting-btn.png'),
     }
